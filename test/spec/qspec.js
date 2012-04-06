a98490a7fdfed27a127543db088d715502463356
David Robbins <robbins.david@gmail.com>
2012-04-06 11:58:10 -0500

    Use Jasmine testing framework

---
+++ test/spec/qspec.js
@@ -0,0 +1,27 @@
+//- JavaScript source code
+
+//- qspec.js ~~
+//
+//  Jasmine (http://pivotal.github.com/jasmine/) BDD Specs for Quanah, written
+//  in an effort to:
+//    * Provide a degree of documentation
+//    * Learn the damn thing
+//    * Provide a way to catch changes made in Quanah's API
+//
+//                                                      ~~ (c) DER, 6 Apr 2012
+
+(function(){
+  "use strict";
+
+  // Pragmas for JSHint
+  /*globals describe:false it:false beforeEach:false expect:false*/
+
+  describe("Quanah", function(){
+
+    it("should be awesome", function(){
+      expect(true).toBeTruthy();
+    });
+
+  });
+
+}());

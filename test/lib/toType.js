67dfb11fb9ec571c3addc4c7e0c1b1fa91344288
Sean Wilkinson <sean@mathbiol.org>
2012-04-09 21:53:42 -0500

    Removed unused libraries from "test" directory (jQuery et al.)

--- test/lib/toType.js
+++
@@ -1,4 +0,0 @@
-// See: https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
-Object.toType = function (obj) {
-    return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
-};

30b81d10d09164a8ea58b1b3ff995068e1beb44b
Sean Wilkinson <sean@mathbiol.org>
2015-03-16 02:45:11 -0500

    Corrected TypeScript definition file

--- src/quanah.d.ts
+++ src/quanah.d.ts
@@ -1,48 +1,48 @@
-//- TypeScript definition file
+//- TypeScript 1.4 source code
 
 //- quanah.d.ts ~~
 //
-//  This file contains a work-in-progress specification of Quanah's public
-//  interfaces using TypeScript. In the future, I hope to integrate Quanah with
-//  the DefinitelyTyped repository (http://definitelytyped.org/).
+//  This file defines Quanah as an "ambient external module" for TypeScript :-)
 //
-//                                                      ~~ (c) SRW, 14 Mar 2015
-//                                                  ~~ last updated 14 Mar 2015
-
-interface AVar {
-    on(name: string, listener: Listener): AVar;
-    Q(f: AVar | Transform): AVar;
-    send(name: string, arg?: any): AVar;
-    val: any;
-}
-
-interface Listener {
-    (message?: any): void;
-}
-
-interface Quanah {
-    avar(val?: any): AVar;
-    canRunRemotely?(task: Task): boolean;
-    runRemotely?(task: Task): void;
-    snooze?(tick: () => void): any;
-    sync(... any): AVar;
-}
-
-interface Task {
-    f: Transform;
-    x: AVar;
-}
-
-interface Transform {
-    call(that: AVar, signal: {
-        exit: Listener;
-        fail: Listener;
-        stay: Listener;
-    }): void;
-}
+//                                                      ~~ (c) SRW, 13 Mar 2015
+//                                                  ~~ last updated 16 Mar 2015
 
 declare module "quanah" {
-    export = Quanah;
+
+    interface AVar {
+        on(name: string, listener: Listener): AVar;
+        Q(f: AVar | Transform): AVar;
+        send(name: string, arg?: any): AVar;
+        val: any;
+    }
+
+    interface Listener {
+        (message?: any): void;
+    }
+
+    interface Task {
+        f: Transform;
+        x: AVar;
+    }
+
+    interface Transform {
+        call(that: AVar, signal: {
+            exit: Listener;
+            fail: Listener;
+            stay: Listener;
+        }): void;
+    }
+
+    export function avar(val?: any): AVar;
+
+    export function canRunRemotely(task: Task): boolean;    // user-provided
+
+    export function runRemotely(task: Task): void;          // user-provided
+
+    export function snooze(tick: () => void): any;          // user-provided
+
+    export function sync(... any): AVar;
+
 }
 
 //- vim:set syntax=typescript:
